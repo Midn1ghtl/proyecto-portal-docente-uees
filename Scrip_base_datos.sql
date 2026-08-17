8e56f658d3d7e65b81d4a563c11e17dccab961f0
danymendez7 <2025010674@cvirtualuees.edu.sv>
2026-08-17 01:51:39 -0600

    Create database schema for educational tables

--- Scrip_base_datos.sql
+++ Scrip_base_datos.sql
@@ -1,3 +1,97 @@
+/* SET SCHEMA 'UniversidadUEES' */
+ 
+/* SQLINES DEMO * able [dbo].[Asignaturas]    Script Date: 16/8/2026 12:17:14 **/
+/* SET ANSI_NULLS ON */
+ 
+/* SET QUOTED_IDENTIFIER ON */
+ 
+-- SQLINES FOR EVALUATION USE ONLY
+CREATE TABLE Asignaturas(
+	AsignaturaId int NOT NULL,
+	Nombre Varchar(50) NULL,
+	CursoId int NULL,
+	ProfesorId int NULL,
+ CONSTRAINT PK_Asignaturas PRIMARY KEY 
+(
+	AsignaturaId
+) 
+);
+ 
+/* SQLINES DEMO * able [dbo].[Calificaciones]    Script Date: 16/8/2026 12:17:17 **/
+/* SET ANSI_NULLS ON */
+ 
+/* SET QUOTED_IDENTIFIER ON */
+ 
+CREATE TABLE Calificaciones(
+	CalificacionId int NOT NULL,
+	AsignaturaId int NULL,
+	EstudianteId int NULL,
+	DocenteId int NULL,
+	Nota1 Double precision NULL,
+	Nota2 Double precision NULL,
+	Nota3 Double precision NULL,
+	Reposicion Double precision NULL,
+	NotaFinal Double precision NULL,
+ CONSTRAINT PK_Calificaciones PRIMARY KEY 
+(
+	CalificacionId
+) 
+);
+ 
+/* SQLINES DEMO * able [dbo].[Cursos]    Script Date: 16/8/2026 12:17:17 **/
+/* SET ANSI_NULLS ON */
+ 
+/* SET QUOTED_IDENTIFIER ON */
+ 
+CREATE TABLE Cursos(
+	CursoId int NOT NULL,
+	Nombre varchar(50) NULL,
+	Horario varchar(20) NULL,
+ CONSTRAINT PK_Cursos PRIMARY KEY 
+(
+	CursoId
+) 
+);
+ 
+/* SQLINES DEMO * able [dbo].[Estudiantes]    Script Date: 16/8/2026 12:17:17 **/
+/* SET ANSI_NULLS ON */
+ 
+/* SET QUOTED_IDENTIFIER ON */
+ 
+CREATE TABLE Estudiantes(
+	EstudianteId int NOT NULL,
+	Nombre varchar(100) NULL,
+	Apellidos varchar(100) NULL,
+	Cif int NULL,
+	Email varchar(150) NULL,
+	MatriculaId varchar(50) NULL,
+	CursoId int NULL,
+	FechaNacimiento Timestamp(3) NULL,
+	Sexo char(1) NULL,
+ CONSTRAINT PK_Estudiantes PRIMARY KEY 
+(
+	EstudianteId
+) 
+);
+ 
+/* SQLINES DEMO * able [dbo].[Profesores]    Script Date: 16/8/2026 12:17:17 **/
+/* SET ANSI_NULLS ON */
+ 
+/* SET QUOTED_IDENTIFIER ON */
+ 
+CREATE TABLE Profesores(
+	ProfesorId int NOT NULL,
+	Nombre varchar(100) NULL,
+	Apellidos varchar(100) NULL,
+	Email varchar(250) NULL,
+	Sexo char(1) NULL,
+	Telefono varchar(15) NULL,
+ CONSTRAINT PK_Docentess PRIMARY KEY 
+(
+	ProfesorId
+) 
+);
+
 CREATE TABLE Evaluaciones(
     EvaluacionesId int NOT NULL
 	AsignaturaId int NOT NULL,
@@ -11,3 +105,8 @@ CREATE TABLE Evaluaciones(
 	AsignaturaId
 ) 
 );
+ 
+ALTER TABLE Asignaturas ADD  CONSTRAINT FK_Asignaturas_Cursos FOREIGN KEY(CursoId)
+REFERENCES Cursos (CursoId);
+ 
+ALTER TABLE Asignaturas VALIDATE CONSTRAINT FK_Asignaturas_Cursos;
